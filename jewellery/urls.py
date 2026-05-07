342340255b1531ca1d2b070d656cdb0f307d7993
Yogita Shete <yogitashete24@gmail.com>
2026-05-07 20:58:30 +0530

    Fix: serve media files in production (DEBUG=False) using static serve view

--- jewellery/urls.py
+++ jewellery/urls.py
@@ -15,11 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('jewellery_products.url')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # Serve media files in both development and production
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
